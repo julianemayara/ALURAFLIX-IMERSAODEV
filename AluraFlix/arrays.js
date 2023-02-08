acfaedbf08db317a8a4231494277121f41cb3703
Juliane Mayara <80775242+julianemayara@users.noreply.github.com>
2023-02-08 14:53:12 -0300

    Add files via upload

---
+++ AluraFlix/arrays.js
@@ -0,0 +1,39 @@
+
+var listaFilmes = [
+
+  "https://th.bing.com/th/id/R.0803475d4faa6cdd550aec539c5dde4a?rik=fBEupLTn9VAKUQ&riu=http%3a%2f%2fbr.web.img2.acsta.net%2fpictures%2f20%2f06%2f15%2f13%2f37%2f0583386.jpg&ehk=D5WD7kHejTEcaecdCjwxcFyaIQrHuF6NESFb5X%2b9ls0%3d&risl=&pid=ImgRaw&r=0",
+
+  "https://th.bing.com/th/id/R.50af530e6cc1c15833b6b3a731fd782c?rik=y1m7Q1Ez7r%2fCqw&riu=http%3a%2f%2ftoutelaculture.com%2fwp-content%2fuploads%2f2016%2f09%2fMr-robot.jpg&ehk=ahxPr0zt%2fT1Ukwzm3fnE%2favhWvEN8HU7L%2f%2fusnPKZJg%3d&risl=&pid=ImgRaw&r=0",
+
+  "https://th.bing.com/th/id/OIP.ZpcclBWlQ9Xy5vkTgfPIkwHaLH?pid=ImgDet&w=758&h=1137&rs=1",
+
+  "https://th.bing.com/th/id/R.2de11b1e2855e7670f3c9b45634162b5?rik=oHikjdVUfpIM8A&pid=ImgRaw&r=0",
+
+  "https://th.bing.com/th/id/R.e07563715a0f6dd8d88a58c3e1f4724a?rik=gouH%2bHCkGZr22A&pid=ImgRaw&r=0",
+
+  "https://th.bing.com/th/id/OIP.MzOFMdJd7iDaDsg6eAQyPgHaLH?pid=ImgDet&rs=1",
+
+  "https://th.bing.com/th/id/OIP.1rjyjRd4FUxxO207taRr7gHaK-?pid=ImgDet&rs=1",
+
+  "https://th.bing.com/th/id/R.b66e3dda3016880ae9d50f35e029d8f7?rik=o11%2fnrNlkkvrbA&riu=http%3a%2f%2fwww.cineol.net%2fgaleria%2fcarteles%2fbigtmp_1063serie.jpg&ehk=KLLvs41Zyz3jCv%2bJHegROXxyAQJtphy9Yg%2biEeWlYBA%3d&risl=&pid=ImgRaw&r=0",
+
+  "https://th.bing.com/th/id/R.c718c3199e9769f43e0f197338f9d422?rik=5CJpdUFrcYq%2bEQ&pid=ImgRaw&r=0",
+
+  "https://th.bing.com/th/id/R.d994266275d660e4552e642271d26f1a?rik=QZWDnnJzBeD3zA&pid=ImgRaw&r=0",
+  
+  "https://www.themoviedb.org/t/p/w440_and_h660_face/iC4KijE4fZdoKsQBluxzADXHf7r.jpg",
+
+  "https://mx.web.img2.acsta.net/pictures/19/10/08/11/04/2148033.jpg",
+
+  "https://www.cine.com/media/series/432_w1000.jpg",
+
+  "https://th.bing.com/th/id/OIP.IANtEIg1227EW3ZAQTDt3gHaKs?pid=ImgDet&rs=1", 
+];
+  
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
+
+console.log(listaFilmes);
+
